6e441bab78ef5401fb6bbc70d7441a6f88212c45
Cocokkkk <e0407630@u.nus.edu>
2021-03-04 10:49:10 +0800

    Kexuan-AboutUs: restore the original one

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -15,11 +15,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
         Scanner in = new Scanner(System.in);
-        printLine();
         System.out.println("Hello " + in.nextLine());
     }
 
-    public static void printLine() {
-        System.out.println("___________________________________________");
-    }
 }

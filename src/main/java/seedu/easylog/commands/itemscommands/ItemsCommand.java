8c43da28ca434b670e177b84a2589b9c16933263
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-09 14:10:13 +0800

    Fix AvoidStarImport

--- src/main/java/seedu/easylog/commands/itemscommands/ItemsCommand.java
+++ src/main/java/seedu/easylog/commands/itemscommands/ItemsCommand.java
@@ -1,6 +1,6 @@
 package seedu.easylog.commands.itemscommands;
 
-import seedu.easylog.item.*;
+import seedu.easylog.item.ItemManager;
 import seedu.easylog.ui.Ui;
 
 public abstract class ItemsCommand {

b10e1bd7cc81c56e1656d1f49d75ec920e241600
Cocokkkk <e0407630@u.nus.edu>
2021-03-10 13:22:03 +0800

    Update Ui: showEmptyName message and showInvalidCommand message

--- src/main/java/seedu/easylog/ui/Ui.java
+++ src/main/java/seedu/easylog/ui/Ui.java
@@ -26,7 +26,9 @@ public class Ui {
     }
 
     public void showInvalidCommand() {
-
+        String invalidCommandMessage = "OOPS!!! I'm Sorry! I don't know what that means.\n" +
+                "Please input again! :)";
+        System.out.println(invalidCommandMessage);
     }
 
     public void showAddItem(Item item) {
@@ -34,6 +36,8 @@ public class Ui {
     }
 
     public void showEmptyName() {
-
+        String emptyNameMessage = "OOPS!!!. The item name is missing!\n + " +
+                "Please complete the information! :) ";
+        System.out.println(emptyNameMessage);
     }
 }

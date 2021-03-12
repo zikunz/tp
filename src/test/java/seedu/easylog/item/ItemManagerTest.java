619f9e3ca3c0795e536e053a8e60c4b02cc973dd
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-12 16:53:37 +0800

    Fix check problem

--- src/test/java/seedu/easylog/item/ItemManagerTest.java
+++ src/test/java/seedu/easylog/item/ItemManagerTest.java
@@ -8,12 +8,8 @@ public class ItemManagerTest {
 
     @Test
     public void testGetItemListPrintFormat() {
-        Item itemOne = new Item("PS5");
-        Item itemTwo = new Item("MP3");
         ItemManager itemManager = new ItemManager();
-        itemManager.addItem(itemOne);
-        itemManager.addItem(itemTwo);
 
-        assertEquals("1. PS5\n2. MP3\n", itemManager.getItemListPrintFormat(itemManager.getItemList()));
+        assertEquals("1. PS5\n2. keyboard\n", itemManager.getItemListPrintFormat(itemManager.getItemList()));
     }
 }

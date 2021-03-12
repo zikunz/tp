f505938cd23b481c67c65c24c7c20b07575578ef
Zhu Zikun @ NUS <zhu.zikun@u.nus.edu>
2021-03-12 20:50:05 +0800

    Add Increment: Help
    [Zikun] This version of easyLog shows 10 other types of commands available and a brief description of the commands.

--- src/main/java/seedu/easylog/common/Messages.java
+++ src/main/java/seedu/easylog/common/Messages.java
@@ -4,4 +4,15 @@ public class Messages {
 
     public static final String MESSAGE_LIST_ITEMS = "Here are the list of items.";
     public static final String MESSAGE_ADD_ITEMS_TO_ORDER = "Input the items to be added to this order.";
+    public static final String MESSAGE_SHOW_ALL_COMMANDS = "General Options:\n"
+            + "  1. items                          Show items-related commands\n"
+            + "  2. items add <item_name>          Add an item\n"
+            + "  3. items list                     List all items\n"
+            + "  4. items remove <item_name>       Remove an item\n"
+            + "  5. items clear                    Empty all items\n"
+            + "  6. orders                         Show orders-related commands\n"
+            + "  7. orders add <order_name>        Add an order\n"
+            + "  8. orders list                    List all orders\n"
+            + "  9. orders remove <order_name>     Remove an order\n"
+            + "  10. orders clear                  Empty all orders\n";
 }

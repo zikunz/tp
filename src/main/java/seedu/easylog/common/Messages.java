59d4860e3ff0fee66a51b555804537de964b502a
Zhu Zikun @ NUS <zhu.zikun@u.nus.edu>
2021-03-12 22:22:26 +0800

    Add Increment: Items-related commands
    [Zikun] This version of easyLog shows 4 other types of items-related commands available and a brief description of the commands.

--- src/main/java/seedu/easylog/common/Messages.java
+++ src/main/java/seedu/easylog/common/Messages.java
@@ -30,5 +30,9 @@ public class Messages {
             + "  2. orders list                    List all orders\n"
             + "  3. orders remove <order_name>     Remove an order\n"
             + "  4. orders clear                   Empty all orders\n";
-
+    public static final String MESSAGE_SHOW_ITEMS_COMMANDS = "Items-Related Options:\n"
+            + "  1. items add <item_name>          Add an item\n"
+            + "  2. items list                     List all items\n"
+            + "  3. items remove <item_name>       Remove an item\n"
+            + "  4. items clear                    Empty all items\n";
 }

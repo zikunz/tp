a5cc91f5b6b6e06c6a31ff0dce0a6c774735cecf
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-15 21:04:45 +0800

    Fix OperatorWraps checkstyle error

--- src/main/java/seedu/easylog/ui/Ui.java
+++ src/main/java/seedu/easylog/ui/Ui.java
@@ -153,7 +153,7 @@ public class Ui {
 
     public void showItemNotFound(int index) {
         int itemNumber = index + Constants.ARRAY_OFFSET;
-        System.out.println("Item number " + itemNumber + " does not exist. " +
-                "Please try another valid item number until at least one item is added to the order.");
+        System.out.println("Item number " + itemNumber + " does not exist. "
+                + "Please try another valid item number until at least one item is added to the order.");
     }
 }

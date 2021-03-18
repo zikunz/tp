c7b7b60f8f1172efd2b93d60a416c6de566b7155
Cocokkkk <e0407630@u.nus.edu>
2021-03-18 16:18:04 +0800

    Add NumberFormatException for delete features

--- src/main/java/seedu/easylog/commands/itemscommands/ItemsDeleteCommand.java
+++ src/main/java/seedu/easylog/commands/itemscommands/ItemsDeleteCommand.java
@@ -8,21 +8,17 @@ public class ItemsDeleteCommand extends ItemsCommand {
     /**
      * Deletes a single item from the list of items.
      */
-    public void execute(String itemsArg) throws EmptyNumberException, InvalidNumberException {
+    public void execute(String itemsArg) throws EmptyNumberException, InvalidNumberException, NumberFormatException {
         if (itemsArg.equals("")) {
             throw new EmptyNumberException();
         }
-        try {
-            int index = Integer.parseInt(itemsArg) - Constants.ARRAY_OFFSET;
-            int size = itemManager.getSize();
-            if ((index < 0) || (index >= size)) {
-                throw new InvalidNumberException();
-            }
-            ui.showDeletedItem(itemManager.getItem(index));
-            itemManager.deleteItem(index);
-            assert itemManager.getSize() == size - 1 : "After a valid deletion, one item is deleted.";
-        } catch (NumberFormatException e) {
-            ui.showNonIntegerItemNumber();
+        int index = Integer.parseInt(itemsArg) - Constants.ARRAY_OFFSET;
+        int size = itemManager.getSize();
+        if ((index < 0) || (index >= size)) {
+            throw new InvalidNumberException();
         }
+        ui.showDeletedItem(itemManager.getItem(index));
+        itemManager.deleteItem(index);
+        assert itemManager.getSize() == size - 1 : "After a valid deletion, one item is deleted.";
     }
 }
--- src/main/java/seedu/easylog/commands/orderscommands/OrdersDeleteCommand.java
+++ src/main/java/seedu/easylog/commands/orderscommands/OrdersDeleteCommand.java
@@ -14,27 +14,22 @@ public class OrdersDeleteCommand extends OrdersCommand {
     /**
      * Deletes a single order from the list of orders.
      */
-    public void execute(String ordersArg) throws EmptyNumberException, InvalidNumberException {
+    public void execute(String ordersArg) throws EmptyNumberException, InvalidNumberException, NumberFormatException {
         if (ordersArg.equals("")) {
             throw new EmptyNumberException();
         }
-        try {
-            int index = Integer.parseInt(ordersArg) - Constants.ARRAY_OFFSET;
-            int size = orderManager.getSize();
-            if ((index < 0) || (index >= size)) {
-                throw new InvalidNumberException();
-            }
-            ui.showOrderDeleted(orderManager.getOrder(index));
-            orderManager.deleteOrder(index);
-            assert orderManager.getSize() == size - 1 : "After a valid deletion, the size decreases by 1";
-            if (size > 1) {
-               assert orderManager.getOrder(orderManager.getSize() - 1) == orderManager.getOrder(size - 2) :
-                       "After a valid deletion, the second last order becomes the last order";
-            }
-        } catch (NumberFormatException e) {
-            ui.showNonIntegerOrderNumber();
+        int index = Integer.parseInt(ordersArg) - Constants.ARRAY_OFFSET;
+        int size = orderManager.getSize();
+        if ((index < 0) || (index >= size)) {
+            throw new InvalidNumberException();
         }
-
+        ui.showOrderDeleted(orderManager.getOrder(index));
+        orderManager.deleteOrder(index);
+        assert orderManager.getSize() == size - 1 : "After a valid deletion, the size decreases by 1";
+        if (size > 1) {
+            assert orderManager.getOrder(orderManager.getSize() - 1) == orderManager.getOrder(size - 2) :
+                    "After a valid deletion, the second last order becomes the last order";
+        }
     }
-
 }
+

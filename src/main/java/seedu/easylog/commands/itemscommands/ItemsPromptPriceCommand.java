d2f1909ee8c686abf351668cea57f0115bc7378a
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-21 14:30:18 +0800

    Update prompt classes to use new methods and remove unneeded exceptions
    - Remove exceptions as NumberFormatException can be used.
    - Used methods in ItemsParser class to process inputs with regards to price and stock

--- src/main/java/seedu/easylog/commands/itemscommands/ItemsPromptPriceCommand.java
+++ src/main/java/seedu/easylog/commands/itemscommands/ItemsPromptPriceCommand.java
@@ -3,12 +3,9 @@ package seedu.easylog.commands.itemscommands;
 import seedu.easylog.common.Constants;
 import seedu.easylog.exceptions.EmptyItemPriceException;
 import seedu.easylog.exceptions.InvalidItemPriceException;
-import seedu.easylog.exceptions.NonNumericItemPriceException;
 import seedu.easylog.exceptions.NullItemPriceException;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.regex.Pattern;
 
 public class ItemsPromptPriceCommand extends ItemsCommand {
     /**
@@ -16,41 +13,20 @@ public class ItemsPromptPriceCommand extends ItemsCommand {
      *
      * @return item price
      */
-    public String execute() throws NullItemPriceException, EmptyItemPriceException, InvalidItemPriceException,
-            NonNumericItemPriceException {
+    public BigDecimal execute() throws NullItemPriceException, EmptyItemPriceException, InvalidItemPriceException {
         ui.promptItemPrice();
         String priceInString = Constants.SCANNER.nextLine();
-
         if (priceInString == null) {
             throw new NullItemPriceException();
         }
-
-        String priceInStringWithoutSpace = priceInString.replace(" ", "");
-        if (priceInStringWithoutSpace.isEmpty()) {
+        if (priceInString.equals("")) {
             throw new EmptyItemPriceException();
         }
-
-        Pattern pattern = Pattern.compile(Constants.REGEX_NUMERIC_INPUT);
-        if (!pattern.matcher(priceInString).matches()) { // If priceInString is not numeric
-            throw new NonNumericItemPriceException();
-        }
-
-        double priceInDouble = Double.parseDouble(priceInString);
-
+        BigDecimal price = itemsParser.processPriceInput(priceInString);
+        Double priceInDouble = price.doubleValue();
         if (priceInDouble < Constants.MINIMUM_ITEM_PRICE || priceInDouble > Constants.MAXIMUM_ITEM_PRICE) {
             throw new InvalidItemPriceException();
         }
-
-        // Float and double are not suitable for monetary calculations
-        // due to loss of significance. Hence, BigDecimal is used
-        BigDecimal bigDecimal = new BigDecimal(priceInString);
-
-        // Set the maximum scale of the item price to be 2
-        bigDecimal = bigDecimal.setScale(Constants.PRICING_SCALE, RoundingMode.HALF_EVEN);
-
-        // Remove all trailing zero(s) of the item price, if any
-        priceInString = bigDecimal.stripTrailingZeros().toPlainString();
-
-        return priceInString;
+        return price;
     }
 }

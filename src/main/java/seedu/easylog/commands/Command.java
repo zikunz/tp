1fb797560e1f1d54c867bc4446b9264bc505bca6
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-23 16:52:37 +0800

    Implement storage feature

--- src/main/java/seedu/easylog/commands/Command.java
+++ src/main/java/seedu/easylog/commands/Command.java
@@ -1,8 +1,10 @@
 package seedu.easylog.commands;
 
+import seedu.easylog.storage.SaveData;
 import seedu.easylog.ui.Ui;
 
 public abstract class Command {
 
     public Ui ui = new Ui();
+    public SaveData saveData = new SaveData();
 }
--- src/main/java/seedu/easylog/parser/Parser.java
+++ src/main/java/seedu/easylog/parser/Parser.java
@@ -1,10 +1,15 @@
 package seedu.easylog.parser;
 
 import seedu.easylog.commands.ExitCommand;
+import seedu.easylog.commands.itemscommands.ItemsAddFromFileCommand;
+import seedu.easylog.commands.orderscommands.OrdersAddFromFileCommand;
 import seedu.easylog.common.Constants;
+import seedu.easylog.exceptions.InvalidFileInputException;
 import seedu.easylog.item.ItemManager;
 import seedu.easylog.order.OrderManager;
 import seedu.easylog.ui.Ui;
+
+import java.io.IOException;
 
 /**
  * Process raw user input.
@@ -18,7 +23,7 @@ public class Parser {
         return splitCommand.length == 2 ? splitCommand : new String[]{splitCommand[0], ""};
     }
 
-    public void processUserInput(String rawUserInput, ItemManager itemManager, OrderManager orderManager) {
+    public void processUserInput(String rawUserInput, ItemManager itemManager, OrderManager orderManager) throws IOException {
         String[] commandTypeAndParams = splitCommandWordAndArgs(rawUserInput);
         String commandType = commandTypeAndParams[0];
         String commandArgs = commandTypeAndParams[1];
@@ -27,7 +32,7 @@ public class Parser {
             ui.showHelp();
             break;
         case (Constants.COMMAND_EXIT):
-            new ExitCommand().execute();
+            new ExitCommand().execute(itemManager, orderManager);
             break;
         case (Constants.COMMAND_ITEMS):
             ItemsParser.processItemsInput(commandArgs, itemManager);
@@ -37,6 +42,20 @@ public class Parser {
             break;
         default:
             ui.showInvalidCommand();
+        }
+    }
+
+    public void processFileInput(String commandType, String commandArgs, ItemManager itemManager,
+                                 OrderManager orderManager) throws InvalidFileInputException {
+        switch (commandType) {
+        case (Constants.COMMAND_ITEMS):
+            new ItemsAddFromFileCommand().execute(commandArgs, itemManager);
+            break;
+        case (Constants.COMMAND_ORDERS):
+            new OrdersAddFromFileCommand().execute(commandArgs, itemManager, orderManager);
+            break;
+        default:
+            throw new InvalidFileInputException();
         }
     }
 }

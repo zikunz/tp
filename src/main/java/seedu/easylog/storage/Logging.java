19970138db57b2bbd0b2d5a612b18113b93927b5
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-24 17:35:27 +0800

    Add logging feature and some logging message

---
+++ src/main/java/seedu/easylog/storage/Logging.java
@@ -0,0 +1,51 @@
+package seedu.easylog.storage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.*;
+
+public class Logging extends Storage {
+
+    Logger logger = Logger.getLogger("easyLogLogs");
+    FileHandler fh;
+
+    public void setUpLogger() {
+        // Reset logs from previous usage of the app
+        LogManager.getLogManager().reset();
+
+        // Do not log to console
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.OFF);
+        logger.addHandler(ch);
+
+        try {
+            File logs = new File("easyLogLogs.txt");
+            if (logs.createNewFile()) {
+                // do nothing as just checking whether logs file exists, else create the logs file
+            }
+            fh = new FileHandler("easyLogLogs.txt");
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            fh.setLevel(Level.ALL);
+            logger.addHandler(fh);
+            logger.info("Logging to file started.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeInfoLevelLog(String logMessage) {
+        logger.setUseParentHandlers(false);
+        logger.info(logMessage);
+    }
+
+    public void writeWarningLevelLog(String logMessage) {
+        logger.setUseParentHandlers(false);
+        logger.warning(logMessage);
+    }
+
+    public void writeSevereLevelLog(String logMessage) {
+        logger.setUseParentHandlers(false);
+        logger.severe(logMessage);
+    }
+}

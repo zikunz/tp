5edb7aa5dd5c85dc113295158fc8827f6e110f7d
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-24 17:43:27 +0800

    Fix AvoidStarImport checkstyle error

--- src/main/java/seedu/easylog/storage/Logging.java
+++ src/main/java/seedu/easylog/storage/Logging.java
@@ -2,7 +2,12 @@ package seedu.easylog.storage;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Logging extends Storage {
 

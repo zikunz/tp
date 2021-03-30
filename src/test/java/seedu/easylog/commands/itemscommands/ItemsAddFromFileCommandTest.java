1bc43f6ff257e914071a3fc4a7434633cc7b06ec
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-30 16:06:44 +0800

    Add ItemsAddFromFileCommand test

---
+++ src/test/java/seedu/easylog/commands/itemscommands/ItemsAddFromFileCommandTest.java
@@ -0,0 +1,30 @@
+package seedu.easylog.commands.itemscommands;
+
+import org.junit.jupiter.api.Test;
+import seedu.easylog.model.Item;
+import seedu.easylog.model.ItemManager;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ItemsAddFromFileCommandTest {
+
+    @Test
+    public void executeItemsAddFromFileTest() {
+        String testItemName = "PS5";
+        BigDecimal testItemPrice = new BigDecimal("599.00");
+        int testItemStock = 10;
+        ItemManager itemManager = new ItemManager();
+        Item testItem = new Item(testItemName, testItemPrice, testItemStock);
+
+        String inputFromFile = "PS5 599.00 10";
+        ItemsAddFromFileCommand itemsAddFromFileCommand = new ItemsAddFromFileCommand();
+        itemsAddFromFileCommand.execute(inputFromFile, itemManager);
+        Item itemAdded = itemManager.getLatestItemAdded();
+
+        assertEquals(testItem.getItemName(), itemAdded.getItemName());
+        assertEquals(testItem.getItemPrice(), itemAdded.getItemPrice());
+        assertEquals(testItem.getItemStock(), itemAdded.getItemStock());
+    }
+}

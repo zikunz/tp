1382f524adc032214261182ac11d9526c625ac72
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-03-30 17:43:41 +0800

    Add processPriceInput and processStockInput test

--- src/test/java/seedu/easylog/parser/ItemsParserTest.java
+++ src/test/java/seedu/easylog/parser/ItemsParserTest.java
@@ -1,4 +1,24 @@
 package seedu.easylog.parser;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ItemsParserTest {
+
+    ItemsParser itemsParser = new ItemsParser();
+
+    @Test
+    public void processPriceInputTest() {
+        String testInput = "599.00";
+        assertEquals(new BigDecimal(testInput), itemsParser.processPriceInput(testInput));
+    }
+
+    @Test
+    public void processStockInputTest() {
+        String testInput = "10";
+        assertEquals(10, itemsParser.processStockInput(testInput));
+    }
 }

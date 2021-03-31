248cf7897d300bc3255329a7b020ae25ca266edd
Yiwen_Qiu <60378963+e00426142@users.noreply.github.com>
2021-03-31 12:48:21 +0800

    Updated jUnitTesting

--- src/test/java/seedu/easylog/order/OrderManagerTest.java
+++ src/test/java/seedu/easylog/order/OrderManagerTest.java
@@ -1,4 +1,39 @@
 package seedu.easylog.order;
 
+import org.junit.jupiter.api.Test;
+import seedu.easylog.model.Item;
+import seedu.easylog.model.ItemManager;
+import seedu.easylog.model.Order;
+import seedu.easylog.model.OrderManager;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OrderManagerTest {
+
+    @Test
+    public void testGetOrderListPrintFormat() {
+        BigDecimal priceOne, priceTwo;
+        ItemManager itemsInOrder = new ItemManager();
+        priceOne = new BigDecimal("100.00");
+        priceTwo = new BigDecimal("36.00");
+        Item itemOne = new Item("PS5",priceOne,5);
+        Item itemTwo = new Item("MP3", priceTwo,5);
+        itemsInOrder.addItem(itemOne);
+        itemsInOrder.addItem(itemTwo);
+        ArrayList<Integer> quantityOfItems = new ArrayList<>();
+        quantityOfItems.add(1);
+        quantityOfItems.add(1);
+        Order orderOne = new Order("Yiwen",itemsInOrder.getItemList(),quantityOfItems);
+        OrderManager orderManager = new OrderManager();
+        orderManager.addOrder(orderOne);
+
+        assertEquals("Yiwen\n"
+                        + "    1. PS5, S$100.00, 1\n"
+                        + "    2. MP3, S$36.00, 1\n"
+                        + "    Total Price : S$136.00\n"
+                        + "____________________________________________________________\n",
+                orderManager.getOrderListPrintFormat());
+    }
 }

05cd90733dcab1f347cbbe7e57750578cf488cf8
Zhu Zikun @ NUS <zhu.zikun@u.nus.edu>
2021-04-10 02:14:30 +0800

    Adding JUnit Tests for Items Add, Items Clear and Items Update

--- src/test/java/seedu/easylog/commands/itemscommands/ItemsAddCommandTest.java
+++ src/test/java/seedu/easylog/commands/itemscommands/ItemsAddCommandTest.java
@@ -1,4 +1,31 @@
 package seedu.easylog.commands.itemscommands;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import seedu.easylog.exceptions.EmptyItemNameException;
+import seedu.easylog.exceptions.NullItemNameException;
+import seedu.easylog.model.ItemManager;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ItemsAddCommandTest {
+    @Test
+    @DisplayName("Items Should Be Added Correctly")
+    public void itemsAddCommand_shouldBeAddedCorrectly() {
+        ItemsAddCommand itemsAddCommand = new ItemsAddCommand();
+        ItemManager itemManager = new ItemManager();
+        ArrayList<String> itemDescriptionRecord = null;
+
+        // Null item description
+        NullItemNameException nullItemNameException = assertThrows(NullItemNameException.class, () -> {
+            itemsAddCommand.execute(null, itemManager, itemDescriptionRecord);
+        });
+
+        // Empty item description
+        EmptyItemNameException emptyItemNameException = assertThrows(EmptyItemNameException.class, () -> {
+            itemsAddCommand.execute("", itemManager, itemDescriptionRecord);
+        });
+    }
 }
